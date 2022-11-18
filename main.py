c52ceb8bd4d21e8a9023c320a41d549a45766f35
maselof <mr.gindeev@mail.ru>
2022-11-18 15:46:33 +0300

    remake_join_reg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import psycopg2
+import requests
 from flask import Flask, url_for, request, redirect, send_file
 from config import user, password, host, port, database
 import controller_site
@@ -14,34 +15,38 @@ UPLOAD_FOLDER = 'resource/'
 app = Flask(__name__)
 
 
-@app.route('/return_file/<filename>')
+
+
+
+@app.route('/api/return_file/<filename>')
 def upload_file(filename):
     file_path = UPLOAD_FOLDER + filename
     return send_file(file_path, as_attachment=True)
 
 
-@app.route('/reg')
+@app.route('/api/reg', methods=['POST'])
 def reg():
-    return controller_site.registration_users('Maselof', 'admin', 'mr.gindeev@mail.ru', '89613960415')
+    data = requests.get('bff').json()
+    return controller_site.registration_users(data.get['username'], data.get['password'],
+                                              data.get['email'], data.get['phone'])
 
 
-@app.route('/join')
+@app.route('/api/join', methods=['POST'])
 def join():
-    response = controller_site.join_user('Maselof', 'admin')
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    data = requests.get('bff').json()
+    return controller_site.join_user(data.get['username'], data.get['password'])
 
-@app.route('/playlists')
+@app.route('/api/playlists')
 def playlists():
     return controller_site.show_user_playlist('Maselof')
 
 
-@app.route('/<id_playlist>/songs')
+@app.route('/api/<id_playlist>/songs')
 def playlist_songs(id_playlist):
     return controller_site.show_songs_playlist(id_playlist)
 
 
-@app.route('/<id_performer>/creativity')
+@app.route('/api/<id_performer>/creativity')
 def performer_playlist(id_performer):
     return controller_site.show_playlist_performer(id_performer)
 
